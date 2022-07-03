d1c727e53db4b4670b148508649a0e2abd33c2e8
Amish <kanyalamish10@gmail.com>
2022-07-03 09:26:49 +0530

    hosting

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import render_template, Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return ("<h1>This is working</h1>")
+
+if __name__ == "__main__":
+    app.run(debug=True)
